1b8d1117d8ee59fe451768c39ac9b29a45e60c73
Maksim Yarotskiy <maksimyarotskiy08@gmail.com>
2025-11-22 20:42:29 +0300

    initial

---
+++ backend/marketfusion/src/main/java/com/marketfusion/MarketFusionApplication.java
@@ -0,0 +1,13 @@
+package com.marketfusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketFusionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketFusionApplication.class, args);
+	}
+
+}
---
+++ backend/marketfusion/src/test/java/com/marketfusion/MarketFusionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketfusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketFusionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
